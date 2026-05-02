b24c0803c8512f1ca6e1889cd94a5a47e76de1e9
youcefrhn <youcefigee@gmail.com>
2026-05-02 17:31:40 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,10 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('PWA Ready ✅'))
+      .catch(err => console.log('SW failed:', err));
+  });
+}
